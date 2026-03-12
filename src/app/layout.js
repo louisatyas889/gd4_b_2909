f3586cdde753281054d097ae54580dd01b12985a
louisatyas889 <louisatyas832@gmail.com>
2026-03-12 14:58:14 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 
-const geintSans = Geist({
+const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
 });
